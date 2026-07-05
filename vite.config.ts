5b1b7264ae12c5c14028db5c0391554d36209502
SAary001 <142585155+Aary23S@users.noreply.github.com>
2026-07-05 23:14:20 +0530

    fix vite production build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          three: ['three', '@react-three/fiber', '@react-three/drei'],
-          motion: ['framer-motion'],
-        },
-      },
-    },
-  },
 });
